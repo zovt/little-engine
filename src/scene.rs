244348973635b82c1d00599d8adf2ad56fe8e7f6
zovt <zovt@posteo.de>
2017-11-17 22:33:04 -0500

    Fix scene call

--- src/scene.rs
+++ src/scene.rs
@@ -1,6 +1,7 @@
 use camera::Camera;
 use chrono::Duration;
 use entity::Entity;
+use transform::Transformation;
 use update::Update;
 
 #[derive(Default)]
@@ -12,6 +13,9 @@ pub struct Scene<'a> {
 impl<'a> Update for Scene<'a> {
 	fn update(&mut self, timestep: Duration) {
 		println!("Timestep: {}", timestep);
-		self.entities.iter_mut().map(|e| e.update(timestep)).count();
+		self.entities
+			.iter_mut()
+			.map(|e| e.update(timestep, Transformation::default()))
+			.count();
 	}
 }
