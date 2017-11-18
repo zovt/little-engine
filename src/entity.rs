8e525754a40d918f1cac695293933b9310bc65a5
zovt <zovt@posteo.de>
2017-11-17 22:32:51 -0500

    Entities update with transform

--- src/entity.rs
+++ src/entity.rs
@@ -1,29 +1,24 @@
 use chrono::Duration;
-use update::Update;
+use physics::PhysicsData;
+use transform::Transformation;
 
-#[derive(Copy, Clone, Hash, Eq, PartialEq)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum EntityType {
 	Node,
+	Transform(Transformation),
 	Drawable,
-	Physics,
+	Physics(PhysicsData),
 }
 
-#[derive(Clone, Hash)]
+#[derive(Clone)]
 pub struct Entity<'a> {
 	name: Option<&'a str>,
 	entity_type: EntityType,
 	children: Vec<Entity<'a>>,
 }
 
-impl<'a> Update for Entity<'a> {
-	fn update(&mut self, timestep: Duration) {
-		println!("{}", self.name.unwrap_or(""));
-		self.children.iter_mut().map(|e| e.update(timestep)).count();
-	}
-}
-
 impl<'a> Entity<'a> {
-	pub fn new(entity_type: EntityType, name: Option<&'a str>) -> Self {
+	pub fn new(name: Option<&'a str>, entity_type: EntityType) -> Self {
 		Self {
 			entity_type,
 			name,
@@ -31,11 +26,27 @@ impl<'a> Entity<'a> {
 		}
 	}
 
-	pub fn physics(name: Option<&'a str>) -> Self {
-		Self::new(EntityType::Physics, name)
+	pub fn physics(name: Option<&'a str>, pd: PhysicsData) -> Self {
+		Self::new(name, EntityType::Physics(pd))
 	}
 
 	pub fn attach(&mut self, entity: Entity<'a>) {
 		self.children.push(entity);
+	}
+
+	pub fn update(&mut self, timestep: Duration, mut transformation: Transformation) {
+		println!("{}", self.name.unwrap_or(""));
+		println!("{:?}", transformation.matrix());
+
+		transformation = if let EntityType::Transform(t) = self.entity_type {
+			transformation * t
+		} else {
+			transformation
+		};
+
+		self.children
+			.iter_mut()
+			.map(|e| e.update(timestep, transformation))
+			.count();
 	}
 }
